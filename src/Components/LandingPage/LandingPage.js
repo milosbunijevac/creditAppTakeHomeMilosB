3da93bdccf55f643585efe28b875d23466e76ba0
Milos Bunijevac <milos.bunijevac@gmail.com>
2021-02-06 18:12:43 -0500

    Added button and form functionality to the landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,50 +1,75 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 
 const LandingPage = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted");
+  };
+
   return (
     <div id="landingPage">
       <Grid container>
         <Grid item xs={2} />
         <Grid item xs={8}>
-          <Grid container id="internalForm" spacing={4}>
-            <Grid item xs={2}>
-              Enter Car Price ($):
+          <form onSubmit={handleSubmit}>
+            <Grid container id="internalForm" spacing={4}>
+              <Grid item xs={2}>
+                Enter Car Price ($):
+              </Grid>
+              <Grid item xs={4}>
+                <TextField />
+              </Grid>
+              <Grid item xs={6} />
+              {/* ------ */}
+              <Grid item xs={2}>
+                Enter Car Make:
+              </Grid>
+              <Grid item xs={4}>
+                <TextField />
+              </Grid>
+              <Grid item xs={6} />
+              {/* ------ */}
+              <Grid item xs={2}>
+                Enter Car Model:
+              </Grid>
+              <Grid item xs={4}>
+                <TextField />
+              </Grid>
+              <Grid item xs={6} />
+              {/* ------ */}
+              <Grid item xs={2}>
+                User Income ($):
+              </Grid>
+              <Grid item xs={4}>
+                <TextField />
+              </Grid>
+              <Grid item xs={6} />
+              {/* ------ */}
+              <Grid item xs={2}>
+                Enter your credit score:
+              </Grid>
+              <Grid item xs={4}>
+                <TextField />
+              </Grid>
+              <Grid item xs={6} />
+              {/* ------ */}
+              <Grid item xs={5} />
+              <Grid item xs={2}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  onSubmit={handleSubmit}
+                >
+                  Apply Now
+                </Button>
+              </Grid>
+              <Grid item xs={5} />
             </Grid>
-            <Grid item xs={4}>
-              <TextField />
-            </Grid>
-            <Grid item xs={6} />
-            <Grid item xs={2}>
-              Enter Car Make:
-            </Grid>
-            <Grid item xs={4}>
-              <TextField />
-            </Grid>
-            <Grid item xs={6} />
-            <Grid item xs={2}>
-              Enter Car Model:
-            </Grid>
-            <Grid item xs={4}>
-              <TextField />
-            </Grid>
-            <Grid item xs={6} />
-            <Grid item xs={2}>
-              User Income ($):
-            </Grid>
-            <Grid item xs={4}>
-              <TextField />
-            </Grid>
-            <Grid item xs={6} />
-            <Grid item xs={2}>
-              Enter your credit score:
-            </Grid>
-            <Grid item xs={4}>
-              <TextField />
-            </Grid>
-            <Grid item xs={6} />
-          </Grid>
+          </form>
         </Grid>
         <Grid item xs={2} />
       </Grid>
