26c0f4339fd00bd2e34b6a36d27ef5b8c7df97ed
Milos Bunijevac <milos.bunijevac@gmail.com>
2021-02-06 20:53:06 -0500

    Implemented react router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,9 @@
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
 import Banner from "../Banner/Banner.js";
 import Footer from "../Footer/Footer.js";
 import LandingPage from "../LandingPage/LandingPage.js";
+import NewAccountPage from "../NewAccount/NewAccount.js";
+import DisqualificationPage from "../DisqualificationPage/DisqualificationPage.js";
 import "./App.css";
 
 function App() {
@@ -8,8 +11,19 @@ function App() {
     <div className="App">
       <Banner />
       <div id="mainSection">
-        {/* Make react router switch here. */}
-        <LandingPage />
+        <Router>
+          <Switch>
+            <Route path="/newaccount">
+              <NewAccountPage />
+            </Route>
+            <Route path="/disqualify">
+              <DisqualificationPage />
+            </Route>
+            <Route path="/">
+              <LandingPage />
+            </Route>
+          </Switch>
+        </Router>
       </div>
       <Footer />
     </div>
--- src/Components/NewAccount/NewAccount.js
+++ src/Components/NewAccount/NewAccount.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
 
 const NewAccount = () => {
   return (
     <div id="newAccount">
-      <div>This is the new account page.</div>
+      <Grid container spacing={2}>
+        <Grid item xs={2} />
+        <Grid item xs={8}>
+          <div>This is the new account page.</div>
+        </Grid>
+        <Grid item xs={2} />
+      </Grid>
     </div>
   );
 };
