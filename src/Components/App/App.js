410f7e3687b78c655eff77a7ead9f1cdc2eabdac
Milos Bunijevac <milos.bunijevac@gmail.com>
2021-02-07 14:47:29 -0500

    Added paper element to enhance visual

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,28 +1,47 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 import Banner from "../Banner/Banner.js";
 import LandingPage from "../LandingPage/LandingPage.js";
 import NewAccountPage from "../NewAccount/NewAccount.js";
 import DisqualificationPage from "../DisqualificationPage/DisqualificationPage.js";
 import "./App.css";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "100%",
+      height: "400px",
+      padding: theme.spacing(2),
+    },
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <Banner />
-      <div id="mainSection">
-        <Router>
-          <Switch>
-            <Route path="/newaccount">
-              <NewAccountPage />
-            </Route>
-            <Route path="/disqualify">
-              <DisqualificationPage />
-            </Route>
-            <Route path="/">
-              <LandingPage />
-            </Route>
-          </Switch>
-        </Router>
+      <div id="mainSection" className={classes.root}>
+        <Paper>
+          <Router>
+            <Switch>
+              <Route path="/newaccount">
+                <NewAccountPage />
+              </Route>
+              <Route path="/disqualify">
+                <DisqualificationPage />
+              </Route>
+              <Route path="/">
+                <LandingPage />
+              </Route>
+            </Switch>
+          </Router>
+        </Paper>
       </div>
     </div>
   );
